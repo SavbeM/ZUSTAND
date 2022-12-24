23e002338c8ebc6bd08b2b2c7eb2e9240da00de8
savbeM <mark.selihov2458@gmail.com>
2022-12-24 14:00:30 +0300

    style fix

--- src/components/allCurrenciesTable.tsx
+++ src/components/allCurrenciesTable.tsx
@@ -34,7 +34,7 @@ export const AllCurrenciesTable = (): any => {
 
         }
         return (
-            <div>
+            <div className="flex flex-col justify-center w-full">
                 <table
                     className="table-auto rounded-md bg-rich-blue ring-dark-blue ring-4 drop-shadow-xl shadow-inner">
                     <thead>
@@ -46,19 +46,17 @@ export const AllCurrenciesTable = (): any => {
                     </thead>
                     <tbody>
                     {renderCurrencyPortion()}
-                    <tr className="text-center align-middle">
-                        <button type="button"
-                                onClick={portionHandler}
-                                className="ring-4 ring-dark-blue text-white bg-grey hover:bg-mint focus:ring-4 focus:ring-light-blue font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 hover:text-black">More...
-                    </button>
-                    </tr>
                     </tbody>
                 </table>
+                <button type="button"
+                        onClick={portionHandler}
+                        className="flex self-center align-middle ring-4 ring-dark-blue text-white bg-grey hover:bg-mint focus:ring-4 focus:ring-light-blue font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 hover:text-black">More...
+                </button>
             </div>
         )
     } else {
         return (
-            <Spinner/>
+                <Spinner/>
         )
     }
 }
