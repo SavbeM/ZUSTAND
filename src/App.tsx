9517f2d382a7baa3a40832a0ebc104609056e4e1
savbeM <mark.selihov2458@gmail.com>
2022-12-26 18:01:00 +0300

    фикс запроса в useEffect + компонент Converter.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,31 @@ import React, {useEffect} from 'react';
 import './App.css';
 import useStore from "./state/mainState";
 import {AllCurrenciesTable} from "./components/allCurrenciesTable";
+import {Converter} from "./components/Converter";
+import {Currencies} from "./types/types";
 ;
 
 
 
-
 function App() {
-    const getCurrencies =useStore((state) => state.getAllCurrencies)
 
+    const getCurrencies = useStore((state) => state.getAllCurrencies)
+    const currencies: Currencies  = useStore((state) => state.currencies)
   useEffect(() => {
-    getCurrencies()
 
+      if(!currencies) {
+        getCurrencies()
+    }
+    else{
+        console.log(currencies)
+    }
   })
 
   return (
     <div className="App">
         <h1 className="text-3xl text-mint font-bold underline">Currencies</h1>
-      <AllCurrenciesTable/>
+        <Converter currencies={currencies}/>
+      <AllCurrenciesTable currencies={currencies}/>
     </div>
   );
 }
