6f9c08a88b7503f4d76629860995da0f68499d92
savbeM <mark.selihov2458@gmail.com>
2022-12-26 18:12:36 +0300

    Не работает метод setCurrency, хуй знает почему. Пишет что тип undefined не равно типу стора.

---
+++ src/state/converterState.ts
@@ -0,0 +1,27 @@
+import create from "zustand";
+import {ConverterStoreType} from "../types/types";
+
+
+ export const useConverterStore = create<ConverterStoreType>()((set) => ({
+  toShow: false,
+    inputSide: null,
+    firstCurrency: null,
+    secondCurrency: null,
+    firstRate: null,
+    secondRate: null,
+
+  show() {set(state => ({...state, toShow: !state.toShow}))},
+     setInputSide(position: string){set(state => ({...state, inputSide: position}))},
+     setCurrency(pos: string, curr: string){set(state => {
+         switch (pos) {
+             case 'left':
+            return  {...state, firstCurrency: curr}
+            break
+             case 'right':
+               return   {...state, secondCurrency: curr}
+            break
+         }
+     })
+     }
+}))
+
--- src/types/types.ts
+++ src/types/types.ts
@@ -6,9 +6,22 @@ export type CurrenciesKeys = {
     exchangedate: string
 }
 
-export type Currencies = CurrenciesKeys[]
+export type Currencies = CurrenciesKeys[] | null
 
 export type MainStoreType = {
     currencies: Currencies | null,
     getAllCurrencies(): void
+}
+
+export type ConverterStoreType = {
+    toShow: boolean,
+    inputSide: string | null,
+    firstCurrency: string | null,
+    secondCurrency: string | null,
+    firstRate: number | null,
+    secondRate: number | null,
+    show():  void,
+    setInputSide(position: string): void,
+    setCurrency(pos: string , curr: string): void
+
 }
