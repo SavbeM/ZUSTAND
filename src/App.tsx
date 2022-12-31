6fdd2f3e26cf980621aadb51c8e73e0e035c8e58
savbeM <mark.selihov2458@gmail.com>
2022-12-31 18:40:43 +0300

    спинер плюс фикс useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
 import React, {useEffect} from 'react';
 import './App.css';
 import useStore from "./state/mainState";
-import {AllCurrenciesTable} from "./components/allCurrenciesTable";
 import {Converter} from "./components/Converter";
+import {Spinner} from "./components/Spinner";
 import {Currencies} from "./types/types";
-;
+import {AllCurrenciesTable} from "./components/AllCurrenciesTable"
 
+    ;
 
 
 function App() {
 
     const getCurrencies = useStore((state) => state.getAllCurrencies)
-    const currencies: Currencies  = useStore((state) => state.currencies)
-  useEffect(() => {
-
-      if(!currencies) {
+    const currencies: Currencies | null = useStore((state) => state.currencies)
+    useEffect(() => {
         getCurrencies()
-    }
-    else{
-        console.log(currencies)
-    }
-  })
+    }, [])
 
-  return (
-    <div className="App">
-        <h1 className="text-3xl text-mint font-bold underline">Currencies</h1>
-        <Converter currencies={currencies}/>
-      <AllCurrenciesTable currencies={currencies}/>
-    </div>
-  );
+    return (
+        <div className="w-full h-full bg-dark text-center">
+            <h1 className="text-3xl text-white p-6 font-bold ">Currency Converter</h1>
+            {currencies ?
+                <div>
+                    <Converter currencies={currencies}/>
+                    <AllCurrenciesTable currencies={currencies}/>
+                </div>
+                :
+                <Spinner/>
+            }
+        </div>
+    );
 }
 
 export default App;
