3862428834736ac23c62140f2126789d901544b8
savbeM <mark.selihov2458@gmail.com>
2023-01-02 20:34:46 +0300

    added countries array and request

--- src/state/mainState.ts
+++ src/state/mainState.ts
@@ -1,14 +1,19 @@
 import create from 'zustand'
 import {MainStoreType} from "../types/types";
-import {getAllCurrenciesRequest} from "../api/requests";
+import {getAllCurrenciesRequest, getCountries} from "../api/requests";
 
 
 
 const useStore = create<MainStoreType>()((set) => ({
     currencies: null,
+    countries: null,
     getAllCurrencies: async () => {
         const response = await getAllCurrenciesRequest()
-        set(() => ({currencies: response}))
+        set((state) => ({...state ,currencies: response}))
+    },
+    getAllCountries: async () => {
+        const response = await getCountries()
+        set((state) => ({...state, countries: response}))
     }
 }))
 
