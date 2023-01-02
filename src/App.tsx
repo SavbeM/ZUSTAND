5bc91a3b2ffe6a254dfb5a8f0ad20c48f7df2b81
savbeM <mark.selihov2458@gmail.com>
2023-01-02 20:28:42 +0300

    getAllCountries request

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,28 @@ import useStore from "./state/mainState";
 import {Converter} from "./components/Converter";
 import {Spinner} from "./components/Spinner";
 import {Currencies} from "./types/types";
-import {AllCurrenciesTable} from "./components/AllCurrenciesTable"
+import {AllCurrenciesTable} from "./components/AllCurrenciesTable";
 
-    ;
+
 
 
 function App() {
 
     const getCurrencies = useStore((state) => state.getAllCurrencies)
+    const getAllCountries = useStore(state => state.getAllCountries )
     const currencies: Currencies | null = useStore((state) => state.currencies)
+    const countries = useStore(state => state.countries)
     useEffect(() => {
         getCurrencies()
+        getAllCountries()
     }, [])
 
     return (
         <div className="w-full h-full bg-dark text-center">
             <h1 className="text-3xl text-white p-6 font-bold ">Currency Converter</h1>
-            {currencies ?
+            {currencies && countries ?
                 <div>
+
                     <Converter currencies={currencies}/>
                     <AllCurrenciesTable currencies={currencies}/>
                 </div>
