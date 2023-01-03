ba78f3ee762af354a44a89b49d3aa256f975fb1c
savbeM <mark.selihov2458@gmail.com>
2023-01-03 16:28:42 +0300

    global error handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React, {useEffect} from 'react';
 import './App.css';
-import useStore from "./state/mainState";
+import {mainStore} from "./state/mainState";
 import {Converter} from "./components/Converter";
 import {Spinner} from "./components/Spinner";
 import {Currencies} from "./types/types";
 import {AllCurrenciesTable} from "./components/AllCurrenciesTable";
+import {getAllCountriesRequest, getAllCurrenciesRequest} from "./api/requests";
 
 
 
 
 function App() {
 
-    const getCurrencies = useStore((state) => state.getAllCurrencies)
-    const getAllCountries = useStore(state => state.getAllCountries )
-    const currencies: Currencies | null = useStore((state) => state.currencies)
-    const countries = useStore(state => state.countries)
+    const getAllCurrencies = mainStore(state => state.getAllCurrencies)
+    const getAllCountries = mainStore(state => state.getAllCountries )
+    const setGlobalError = mainStore(state => state.setGlobalError)
+    const globalError = mainStore(state => state.globalError)
+    const currencies = mainStore((state) => state.currencies)
+    const countries = mainStore(state => state.countries)
+
+    const getCurr = async () => {
+        try {
+            const data: Currencies = await getAllCurrenciesRequest()
+            getAllCurrencies(data)
+        }
+        catch (e) {
+            setGlobalError(e)
+            console.log(e)
+        }
+    }
+    const getCnt = async () => {
+        try {
+            const data: any = await getAllCountriesRequest()
+            getAllCountries(data)
+        }
+        catch (e) {
+            setGlobalError(e)
+            console.log(e)
+        }
+    }
+
     useEffect(() => {
-        getCurrencies()
-        getAllCountries()
+        getCurr()
+        getCnt()
     }, [])
 
-    return (
+return (!globalError) ?  (
         <div className="w-full h-full bg-dark text-center">
             <h1 className="text-3xl text-white p-6 font-bold ">Currency Converter</h1>
             {currencies && countries ?
@@ -33,7 +58,14 @@ function App() {
                 <Spinner/>
             }
         </div>
-    );
+    )
+    :
+    <div className="w-full h-full bg-dark text-center">
+        <h1 className="text-3xl text-red-600 p-6 font-bold ">Error {globalError.message}!!!</h1>
+    </div>
+
+
+
 }
 
 export default App;
