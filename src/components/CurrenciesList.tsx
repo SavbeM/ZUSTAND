2e29ef58d4e7138844e15371fa0bcaa4d34b36e8
savbeM <mark.selihov2458@gmail.com>
2023-01-03 16:33:54 +0300

    import fix

--- src/components/CurrenciesList.tsx
+++ src/components/CurrenciesList.tsx
@@ -2,13 +2,13 @@ import {CurrenciesKeys, CurrenciesListProps} from "../types/types";
 import {useConverterStore} from "../state/converterState";
 import {currenciesListContainerS, currenciesListItemS} from "../styles/styles";
 import React from "react";
-import useStore from "../state/mainState";
+import {mainStore} from "../state/mainState";
 
 
 export const CurrenciesList = (props: CurrenciesListProps) => {
     const setCurrencyName: (pos: string, curr: string, rate: number) => void = useConverterStore(state => state.setCurrencyName)
     const setIsShown = useConverterStore(state => state.setIsShown)
-    const countries = useStore(state => state.countries)
+    const countries = mainStore(state => state.countries)
     const getFlag = (currItem: CurrenciesKeys) => {
        return  countries.map((i: any) => {
            let code: string | number
@@ -25,7 +25,7 @@ export const CurrenciesList = (props: CurrenciesListProps) => {
             }
 
             if (i.numericCode == code) {
-                return <img className='w-5' src={i.flag}/>
+                return <img key={Math.random()} className='w-8 align-middle self-center' src={i.flag}/>
             }
         })
     }
@@ -34,12 +34,12 @@ export const CurrenciesList = (props: CurrenciesListProps) => {
         <div onBlur={(e) => setIsShown()}
              className={currenciesListContainerS}
              role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex={-1}>
-            <div className="py-1 max-h-24 overflow-y-auto" role="none">
-                {props.currencies?.map((currItem) => <div
+            <div className="flex flex-col py-1 max-h-24 overflow-y-auto justify-center align-middle" role="none">
+                {props.currencies.map((currItem) => <div
                     onClick={() => setCurrencyName(props.position, currItem.cc, currItem.rate)}
-                    key={currItem.cc} className={currenciesListItemS}
+                    key={Math.random()} className={currenciesListItemS}
                 >
-                    <div>{currItem.cc}</div>
+                    <div key={currItem.cc}>{currItem.cc}</div>
                     {getFlag(currItem)}
                 </div>)}
             </div>
